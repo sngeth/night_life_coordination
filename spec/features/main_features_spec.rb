0afc7c5f0ed5742f44dc4f3a540d1e9cc1948893
Sid Ngeth <sngeth@gmail.com>
2016-09-13 17:32:28 -0400

    Generate bar resource

--- spec/features/main_features_spec.rb
+++ spec/features/main_features_spec.rb
@@ -17,9 +17,12 @@ RSpec.describe "Main Features", type: :feature do
     end
   end
 
-#User Story: As an authenticated user, I can add myself to a bar to indicate I am going there tonight.
+#User Story: As an authenticated user,
+#I can add myself to a bar to indicate I am going there tonight.
 
-#User Story: As an authenticated user, I can remove myself from a bar if I no longer want to go there.
+#User Story: As an authenticated user,
+#I can remove myself from a bar if I no longer want to go there.
 
-#User Story: As an unauthenticated user, when I login I should not have to search again.
+#User Story: As an unauthenticated user,
+#when I login I should not have to search again.
 end
